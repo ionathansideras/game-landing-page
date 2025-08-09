897bed31558593fcad9399b115848ce5ae6d1aa7
Ionathan Sideras <iona8ansideras@gmail.com>
2025-08-09 20:51:51 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <main className="bg-amber-700">
+            <p className="text-5xl font-general text-violet-300">hello</p>
+        </main>
+    );
+}
+
+export default App;
