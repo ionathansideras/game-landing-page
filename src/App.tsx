37b7640d1dcda027f67df2af92a5044b09553887
Ionathan Sideras <iona8ansideras@gmail.com>
2025-08-20 18:40:15 +0300

    switch vw with % for better layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Story from "./components/Story";
 
 function App() {
     return (
-        <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <main className="relative min-h-screen w-full overflow-x-hidden">
             <NavBar />
             <Hero />
             <About />
